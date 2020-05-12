722671dfd379648d94a3dae16291fb9bd8a292f3
tiderjian <tider@quansitech.com>
2020-05-12 17:11:16 +0800

    降低js打包文件的大小

--- src/QrscannerProvider.php
+++ src/QrscannerProvider.php
@@ -9,6 +9,7 @@ class QrscannerProvider implements Provider{
 
     public function register(){
         RegisterContainer::registerSymLink(WWW_DIR . '/Public/qrscanner', __DIR__ . '/../js/dist');
+        RegisterContainer::registerHeadJs(__ROOT__ . '/Public/qrscanner/qrscanner-bundle.js', true);
 
         RegisterContainer::registerListRightButtonType('qrscanner', QrcodeScanner::class);
     }
